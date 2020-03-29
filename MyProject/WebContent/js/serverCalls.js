115dadf8cb0b9af623d34f6e1dfcaed1327cf3e2
LogosItAcademy <Tester99037@gmail.com>
2020-03-29 23:27:31 +0300

    Initial commmit

--- MyProject/WebContent/js/serverCalls.js
+++ MyProject/WebContent/js/serverCalls.js
@@ -23,3 +23,16 @@ $("button.createProduct")
 								});
 					
 				});
+
+$("button.buy-product").click(function() {
+	var productId = jQuery(this).attr("product-id");
+	
+	
+	$.post("bucket", {'productId':productId},
+			function(data) {
+				if (data == 'Success') {
+					$('#buyProductModal').hide();
+					alert('Success');
+				}
+			});
+});
